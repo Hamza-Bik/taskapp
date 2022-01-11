558f9d356653d45efdabae80177dac44435e49cb
Hamza-Bik <49659116+Hamza-Bik@users.noreply.github.com>
2022-01-11 23:59:00 +0100

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+// type Style=[2]|'bold';
+
+// let font:Style;
+
+// font=[2];
+
