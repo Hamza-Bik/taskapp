829168abaf7d5d92daebfd2b70ff6db77398ffed
Hamza-Bik <49659116+Hamza-Bik@users.noreply.github.com>
2022-01-12 02:20:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/taskapp'));
+app.use(express.static(__dirname + '/dist/angular-task-app'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/taskapp/index.html'));});
+    '/dist/angular-task-app/index.html'));});
 app.listen(process.env.PORT || 8080);
